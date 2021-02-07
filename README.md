7ba6b8ae3e3a12058f9b780bd738b0a45bb50c32
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-07 11:24:13 +0600

    display items successfully

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# foodies-terminal
+<h1>Foodies Terminal</h1>
+<p>A simple food items website where you can find food items by searching their first letter.</p>
+<p><a href="https://pallabbarman.github.io/foodies-terminal/">Live Site</a></p>
--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ document.getElementById('search-item').addEventListener('click', () => {
     const foodInput = document.getElementById('food').value;
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodInput}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayFood(data.meals))
 })
+
+// display food picture and name 
+const displayFood = items => {
+    const showFood = document.getElementById('items');
+    items.forEach(item => {
+        const foodItem = document.createElement('div');
+        foodItem.className = 'food-item';
+        const foodInfo = `
+        <img src="${item.strMealThumb}"><br><br>
+        <h4 class="foodItems">${item.strMeal}</h4><br>`;
+        foodItem.innerHTML = foodInfo;
+        showFood.appendChild(foodItem);
+    });
+}
