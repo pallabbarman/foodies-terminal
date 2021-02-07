cee45e0edb8aed369a167bbb3f27394ad93be65f
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-07 13:39:37 +0600

    details part added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // find food item by 1st letter 
 document.getElementById('search-item').addEventListener('click', () => {
-    const foodInput = document.getElementById('food').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodInput}`)
+    const foodName = document.getElementById('food').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
         .then(res => res.json())
         .then(data => displayFood(data.meals))
+        .catch(error => alert("Please search foods by their first letter!"));
+    document.getElementById('food').value = "";
 })
 
 // display food picture and name 
@@ -14,8 +16,28 @@ const displayFood = items => {
         foodItem.className = 'food-item';
         const foodInfo = `
         <img src="${item.strMealThumb}"><br><br>
-        <h4 class="foodItems">${item.strMeal}</h4><br>`;
+        <h4>${item.strMeal}</h4><br>
+        <button onclick="displayFoodDetail('${item.idMeal}')">Details</button>`;
         foodItem.innerHTML = foodInfo;
         showFood.appendChild(foodItem);
     });
+}
+
+// food items details call by another api 
+const displayFoodDetail = id => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => foodDetailInfo(data.meals[0]))
+        .catch(error => alert("Please search foods by their first letter!"));
+}
+
+// details foodItem info 
+const foodDetailInfo = foods=>{
+    document.getElementById('items').style.display = "none";
+    document.getElementById('itemsDetails').style.display = "block";
+    const foodItem=document.getElementById('itemsDetails');
+    foodItem.innerHTML=`
+    <img src="${foods.strMealThumb}">
+    <h4>${foods.strMeal}</h4>`;
 }
